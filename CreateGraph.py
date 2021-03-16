baf337e12741159ddac28d58cd3746a056145cb1
Brian Oslinker <no>
2021-03-16 22:20:24 +1100

    -batch processing now saves to the correct folder

--- CreateGraph.py
+++ CreateGraph.py
@@ -10,9 +10,9 @@ Created on Tue Mar 16 15:17:25 2021
 import tkinter as tk
 from tkinter import filedialog
 from pathlib2 import Path
+import os
 import pandas as pd
 import ProcessXPSData as xps
-import os
 
 # import data
 root = tk.Tk()
@@ -21,23 +21,24 @@ directory = filedialog.askdirectory(
     title='Select Data Folder')                            # asks user for file
 output = filedialog.askdirectory(title='Save As Folder')   # asks where to save
 root.destroy()                                             # removes window
-# %%
+
 pathlist = Path(directory).rglob('*.dat')
-# %%
+
 for path in pathlist:
-    # %%
+
     # creates data frame with file
     df = pd.read_table(str(path), delimiter=' ')
-# %%
+
     # choose data file
     df = xps.ProcessData(df)
-# %%
+
     # checks what ploting function to call
     if df['Binding Energy'].mean() > 260:
         fig = xps.PlotC1s(df)
     else:
         fig = xps.PlotOther(df)
-# %%
+
     # saves as .svg
-    fig.savefig(os.path.join(
-        output, os.path.splitext(str(path))[0]+".svg"))
+    filename = os.path.basename(str(path))
+    filename = os.path.splitext(filename)[0]
+    fig.savefig(os.path.join(output, filename+".svg"))
