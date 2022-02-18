8463ac9f5738690887f07013f9201478153f64b1
Brian Oslinker <brian.oslinker@gmail.com>
2022-02-18 23:46:09 +1100

    Various Improvements
    Hopefully more to come

--- Dipole.py
+++ Dipole.py
@@ -16,12 +16,15 @@ import numpy as np
 
 # import data
 root = tk.Tk()
+root.attributes('-topmost', True)                       # forces window to top
 root.withdraw()                                           # hides empty window
 file = filedialog.askopenfile(
     title='Select Data Folder')                           # asks user for files
 output = filedialog.askdirectory(
     title='Select Save Folder')                           # asks where to save
 root.destroy()
+
+Scale = 0.4e13
 
 # read data
 df = pd.read_csv(file)
@@ -44,19 +47,19 @@ with plt.style.context(['seaborn-colorblind']):
     plt.plot(x, m*x, linestyle=':', label='Best Fit')
 
     for i in range(0, df.shape[0]):
-        plt.text(x[i]+0.5e12, y[i], str(df.iloc[i, 0])+' ML')
+        plt.text(x[i]+((x.max()*.025)), y[i], str(df.iloc[i, 0])+' ML')
 
     # label Chart
     plt.legend(loc='upper left')
     plt.title('Change in Surface Dipole Potential')
     ax1.set_xlabel('Aeral Density N$^-_A cm^{-2}$')  # global
     ax1.set_ylabel('Change in Surface Dipole (eV)')  # global
-    plt.xlim(right=x.max()+0.4e13)
+    plt.xlim(right=x.max()+(x.max()*.15))
     plt.tight_layout()
 
 # saves as .svg with same name as the .dat file
 filename = os.path.basename(str(file))          # removes path from file
 filename = os.path.splitext(filename)[0]        # removes .dat from file
 fig.savefig(os.path.join(output, filename + ".svg"))  # saves as svg
-filename = os.path.splitext(filename)[0]        # removes .dat from file
-fig.savefig(os.path.join(output, filename + ".svg"))  # saves as svg
+# filename = os.path.splitext(filename)[0]        # removes .dat from file
+# fig.savefig(os.path.join(output, filename + ".svg"))  # saves as svg
